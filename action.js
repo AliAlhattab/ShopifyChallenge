ec4d207842b279d5f587077831d6e902d52b6f6f
AliAlhattab <96435371+AliAlhattab@users.noreply.github.com>
2022-01-18 19:15:25 -0500

    cleaned up code

--- action.js
+++ action.js
@@ -1,4 +1,4 @@
-function fetchdata(){
+function fetchSpacePhoto(){
     fetch("https://api.nasa.gov/EPIC/api/natural/images?api_key=RzuJuaNCpoiAXpt5HRDYfZj49dxsVJMFQsbWNE1s")
     .then(response => {
         return response.json();
@@ -6,40 +6,37 @@ function fetchdata(){
     .then(data => {
         
        const html = data.map(data => {
-          let myDate = new Date(data.date);
+
+          //extract date fields to build api url for nasa photo   
+          let myDate = new Date(data.date); 
           let months = ["01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12"];
           let year = myDate.getFullYear();
           let month = months[myDate.getMonth()];
           let day = myDate.getDate();
-
+          
+          // create and set html code for respective space photo
           return `
             <div class="info1">
                 <p><img style="width: 500px;"src="https://api.nasa.gov/EPIC/archive/natural/${year}/${month}/${day}/png/${data.image}.png?api_key=RzuJuaNCpoiAXpt5HRDYfZj49dxsVJMFQsbWNE1s" alt="${"Image"}"/></p>
                 <p>${data.date}</p>
                 <p>${data.caption}</p>
-                <button class="btn" onclick="btn1(this)">Like</button>
+                <button class="btn" onclick="toggleLike(this)">Like</button>
             </div>
-           `;
-  
+           `
        }).join(" ");
-       console.log(html);
-       document
-       .querySelector("#nasa")
-       .insertAdjacentHTML("afterbegin", html);
-    })
-    
-   
-    
-}
-fetchdata();
 
-function btn1(info) {
-if(info.innerHTML == "Like") {
-info.innerHTML = "❤️"
-
-}
-else if(info.innerHTML == "❤️"){
-info.innerHTML = "Like"
-}
+       //append space photo html to page
+       document.querySelector("#nasa").insertAdjacentHTML("afterbegin", html);
+    })  
 }
 
+function toggleLike(info) {
+    if(info.innerHTML == "Like") {
+        info.innerHTML = "❤️"
+    }
+    else if(info.innerHTML == "❤️"){
+        info.innerHTML = "Like"
+    }
+}
+
+fetchSpacePhoto();
